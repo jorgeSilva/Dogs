d1c7838853a15265f17dcb909588ecfc812a1ca6
Jorge Silva <jorgelfsilva5@gmail.com>
2023-03-13 11:51:08 -0300

    Adição da rota de cadastro junto a estilização e funcionalidade

--- src/Components/Login/LoginCreate/LoginCreate.js
+++ src/Components/Login/LoginCreate/LoginCreate.js
@@ -1,13 +1,42 @@
 import React from 'react'
+import { USER_POST } from '../../../api/Api'
+import useFetch from '../../../Hooks/useFetch'
+import useForm from '../../../Hooks/useForm'
+import { UserContext } from '../../../UserContext'
+import Button from '../../Forms/Button/Button'
+import Input from '../../Forms/Input/Input'
+import Erro from '../../Helper/Erro'
 
 const LoginCreate = () => {
-  function handleSubmit(){}
+  const username = useForm()
+  const email = useForm('email')
+  const password = useForm()
+
+  const {userLogin} = React.useContext(UserContext)
+  const {loading, error, request} = useFetch()
+
+  async function handleSubmit(e){
+    e.preventDefault()
+    const {url, options} = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value
+    })
+    const {response} = await request(url, options)
+
+    if(response.ok) userLogin(username.value, password.value)
+    console.log(response);
+  }
 
   return (
     <section className='animeLeft'>
       <h1 className='title'>Cadastre-se</h1>
       <form onSubmit={handleSubmit}>
-        
+        <Input label='Usuario' type='text' name='username' {...username}/>
+        <Input label='Email' type='email' name='email' {...email}/>
+        <Input label='Senha' type='password' name='password' {...password}/>
+        {loading ? <Button disabled>Cadastrando...</Button>:<Button>Cadastrar</Button> }
+        <Erro erro={error} />
       </form>
     </section>
   )
--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -4,6 +4,10 @@ const types = {
   email:{
     regex: /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
     message: 'Preencha um email válido'
+  },
+  password:{
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+    message: 'A senha precisa conter pelo menos 1 caracter maíusculo, 1 minúsculo e 1 digito. Com no minimo 8 caracteres.'
   }
 }
 
